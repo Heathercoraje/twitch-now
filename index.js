429548abfdef350de38f2387821a05a905074c98
heathercoraje <yhk7830@hotmail.com>
2018-01-30 19:26:37 +0900

    data fectching success

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 
+var base = 'https://wind-bow.gomix.me/twitch-api/streams/';
+var jsonp ='?callback=?';
+var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+
+function makeUrl(user) {
+  var url = base + user + jsonp;
+  console.log(url);
+  return url
+}
+
 // Script and JSONP requests are not subject to the same origin policy restrictions.
-var url = 'https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?';
-$.getJSON(url, function (data) {
-  console.log(data.stream);
-  console.log('data has been loaded');
-})
+function fetchData (url) {
+  $.getJSON(url, function (data) {
+    console.log(data.stream);
+    console.log('data has been loaded');
+  });
+}
+
+users.forEach(function(user) {
+  fetchData(makeUrl(user));
+});
