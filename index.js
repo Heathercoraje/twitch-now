35e42f9c89678ded6d340d077442d82cb6fe6e13
heathercoraje <yhk7830@hotmail.com>
2018-01-31 19:47:54 +0900

    adding online channels

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-
-var base = 'https://wind-bow.gomix.me/twitch-api/channels/';
-var jsonp ='?callback=?';
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-
 function makeUrl(user) {
   var url = base + user + jsonp;
   console.log(url);
@@ -19,14 +14,39 @@ function fetchData(url, callback) {
 }
 
 
+(function goGetThem() {
+  // load all the data before hand
+  // then simply append them to list
+  // click event does not have to handled thanks to bootstrap nav-tab
+  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var base = 'https://wind-bow.gomix.me/twitch-api/';
+  var jsonp ='?callback=?';
+
+  function makeUrl(type, user) { //type is either channels or streams
+    return base + type + '/' + user + jsonp;
+  }
+
+  users.forEach(function (user) {
+    $.getJSON(makeUrl('streams', user), function(data) {
+      var status;
+      if (data.stream === null) {
+        status = 'offline';
+      } else { // if it is streaming
+        status = 'online'
+        var display = JSON.stringify(data.stream.game);
+        $( '#online').append(`<div>${display}</div>`)
+      }
+
+    });
+  });
+})();
+
 $( '#all' ).on( 'click', function() {
   users.forEach(function (user) {
     fetchData(makeUrl(user), function(data) {
-      var data = data;
+      var online;
       console.log(data);
-      // console.log(data.stream);
-      // console.log(data.stream.game);
-      // $( '#allList').append(`<li>${data.streams.game}</li>`);
+      $( '#allList').append(`<li>${data.game}</li>`);
     });
   });
 });
