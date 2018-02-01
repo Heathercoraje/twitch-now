3a561b0bd1f88e13b7079b5069ff8d094c593476
heathercoraje <yhk7830@hotmail.com>
2018-02-01 18:37:43 +0900

    adds to dropdown menu channels need to be generated in search tab

--- index.js
+++ index.js
@@ -1,74 +1,94 @@
-var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
-var base = 'https://wind-bow.gomix.me/twitch-api/';
-var jsonp ='?callback=?';
-var $online = $( '#online');
-var $offline = $( '#offline');
-var $all = $( '#all');
+(function allInOne() {
+  var users = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+  var base = 'https://wind-bow.gomix.me/twitch-api/';
+  var jsonp ='?callback=?';
+  var $online = $( '#online');
+  var $offline = $( '#offline');
+  var $search = $( '#search');
+  var $all = $( '#all');
+  var $searchForm = $('#searchForm');
+  var $suggestion = $('#suggestion');
 
-function makeUrl(type, user) { //type is either channels or streams
-  return base + type + '/' + user + jsonp;
-}
-
-function getAllDataForUser (user) {
-  return new Promise( function (resolve, reject) { // promise takes 2 function arguments
-    $.getJSON(makeUrl('streams', user), function (data) {
-      // add meta info to return object(data)
-      data.status = (data.stream === null) ? 'offline' : 'online';
-      $.getJSON(makeUrl('channels', user), function (response) {
-        data.channels = response;
-        resolve(data); // return data
-      });
-    });
-  })
-}
-
-// apply this function to all users then this will return 8 promises
-var allData = users.map(getAllDataForUser);
+  // $searchForm.keydown(function() {
+  //   var key = $searchForm.val();
+  //   var filtered = users.filter(user => user.indexOf(key) !== -1);
+  //   suggestion.innerHTML= '';
+  //   filtered.forEach(function (user) {
+  //     var option = document.createElement('option');
+  //     option.value = user;
+  //     suggestion.appendChild(option);
+  //   });
+  // });
 
 
-// Promise.all(iterable) will wait 8 promises to be completed and pass it pending to allResponse
-Promise.all(allData).then(function(allResponses) {
-
-  var onlineRes = allResponses.filter(r => r.status === 'online').map(r => getMarkupForUser(r, 'online'));
-  $online.append(onlineRes);
-
-  var offlineRes = allResponses.filter(r => r.status === 'offline').map(r => getMarkupForUser(r, 'offline'));
-  $offline.append(offlineRes);
-
-  var allRes = allResponses.map(r => getMarkupForUser(r, r.status));
-  $all.append(allRes);
-});
-
-function getMarkupForUser (user, isOnline) {
-// here user is each user data object
-// now user.channels has channels now, create elements that I want to append to browswer
-  var $div = $('<div/>', {
-    class :'item'
-  });
-
-  var $img = $('<img/>', {
-    class : 'logoImage rounded-circle',
-    src : user.channels.logo
-  });
-
-  var $p = $('<p/>', {
-    class : `detail name ${isOnline} ? 'online' : 'offline'`,
-    text  : user.channels.display_name + user.channels.status
-  });
-
-  var $a =$('<a/>', {
-    href : user.channels.url,
-    target : '_blank'
-  });
-
-  var $i = $('<i/>', {
-    class : 'fa fa-circle'
-  });
-
-  var $result = $div.append($img).append($p);
-  if (isOnline === 'online') {
-    $a.append($i),
-    $result.append($a)
+  function makeUrl(type, user) { //type is either channels or streams
+    return base + type + '/' + user + jsonp;
   }
-  return $result;
+
+  function getAllDataForUser (user) {
+    return new Promise( function (resolve, reject) { // promise takes 2 function arguments
+      $.getJSON(makeUrl('streams', user), function (data) {
+        // add meta info to return object(data)
+        data.status = (data.stream === null) ? 'offline' : 'online';
+        $.getJSON(makeUrl('channels', user), function (response) {
+          data.channels = response;
+          resolve(data); // return data
+        });
+      });
+    })
+  }
+
+  // apply this function to all users then this will return 8 promises
+  var allData = users.map(getAllDataForUser);
+
+
+  // Promise.all(iterable) will wait 8 promises to be completed and pass it pending to allResponse
+  Promise.all(allData).then(function(allResponses) {
+
+    var onlineRes = allResponses.filter(r => r.status === 'online').map(r => getMarkupForUser(r, 'online'));
+    $online.append(onlineRes);
+
+    var offlineRes = allResponses.filter(r => r.status === 'offline').map(r => getMarkupForUser(r, 'offline'));
+    $offline.append(offlineRes);
+
+    var allRes = allResponses.map(r => getMarkupForUser(r, r.status));
+    $all.append(allRes);
+  });
+
+  function getMarkupForUser (user, isOnline) {
+    // here user is each user data object
+    // now user.channels has channels now, create elements that I want to append to browswer
+    var $div = $('<div/>', {
+      class :'item'
+    });
+
+    var $img = $('<img/>', {
+      class : 'logoImage rounded-circle',
+      src : user.channels.logo
+    });
+
+    var $p = $('<p/>', {
+      class : `detail name ${isOnline} ? 'online' : 'offline'`,
+      text  : user.channels.display_name + user.channels.status
+    });
+
+    var $a =$('<a/>', {
+      href : user.channels.url,
+      target : '_blank'
+    });
+
+    var $i = $('<i/>', {
+      class : 'fa fa-circle'
+    });
+
+    var $result = $div.append($img).append($p);
+    if (isOnline === 'online') {
+      $a.append($i),
+      $result.append($a)
+    }
+    return $result;
+  }
+
+
 }
+)()
